de381787967495a6af05fac69ea28fe7db541f95
Bảo Trần <billtrancon12@gmail.com>
2023-11-26 23:33:20 -0600

    fix homepage

--- src/pages/mainpage.js
+++ src/pages/mainpage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import ExploreButton from "../components/exploreButton";
 import RandomPlaylistButton from "../components/randomPlaylistButton";
 import QuizButton from "../components/quizButton";
@@ -8,6 +8,7 @@ import { useState } from "react";
 import axios from "axios";
 import "../css/homepage.css"
 import MoreButton from "../components/moreButton";
+import LoadingPage from "./loading";
 
 const Homepage = () =>{
     const [blogs, setBlogs] = useState([])
@@ -15,66 +16,77 @@ const Homepage = () =>{
     const [moreButton, setMoreButton] = useState("hide")
     const [loadFrom, setLoadFrom] = useState((sessionStorage.getItem('load from') === null) ? 0 : parseInt(sessionStorage.getItem("load from"), 10))
     const [loadEnd, setLoadEnd] = useState((sessionStorage.getItem("last fetched") === null) ? 2 : parseInt(sessionStorage.getItem("last fetched"), 10))
-    const [changeCount, setChangeCount] = useState(0);
+    const latestId = useRef((sessionStorage.getItem('max blogs') === null) ? 0 : sessionStorage.getItem('max blogs'))
+    const blogsArr = useRef([])
+    const BLOGS_SIZE = useRef(0)
+    const [blogCount, setBlogCount] = useState(-1)
+
     useEffect(() => {
         // sessionStorage.clear()
         if(!isFetch && (sessionStorage.getItem("fetched") === null || sessionStorage.getItem('fetched') === 'null')){
             async function fetchData(){
                 await axios.get(`/.netlify/functions/getBlog/blogs/?rangeFrom=${loadFrom}&rangeEnd=${loadEnd}`).then(async (res)=>{
                     const response = JSON.parse(res.data)
-                    let blogsArr = []
                     if(response.body === "Empty") return
                     let dataArr = response.message.body.data
-                    let latestId = response.message.body.latestId
+                    latestId.current = response.message.body.latestId
+                    BLOGS_SIZE.current = BLOGS_SIZE.current + loadEnd - loadFrom + 1
 
                     for(let i = 0; i < dataArr.length; i++){
                         const data = dataArr[i]
                         let imgData;
                     
-                        await axios.get(`/.netlify/functions/getBlog/images/?filename=${data.image}`).then((res)=>{
+                        axios.get(`/.netlify/functions/getBlog/images/?filename=${data.image}`).then((res)=>{
                             const result = JSON.parse(res.data)
                             imgData = result.image
+                            const blog = 
+                            <BlogWrapper
+                                href={`/blog/${data.topic}`}
+                                src={`data:image/png;base64,${imgData}`}
+                                topic={data.topic}
+                                content={data.content}
+                                key={i}
+                            ></BlogWrapper>
+                            blogsArr.current.push(blog)
+                            setBlogCount(blogsArr.current.length)
                         }).catch((err)=>console.log(err))
 
-                        const blog = 
-                        <BlogWrapper
-                            href={`/blog/${data.topic}`}
-                            src={`data:image/png;base64,${imgData}`}
-                            topic={data.topic}
-                            content={data.content}
-                            key={i}
-                        ></BlogWrapper>
-                        blogsArr.push(blog)
-                        setBlogs(blogsArr)
-                        setChangeCount(changeCount + 1)
+                        // const blog = 
+                        // <BlogWrapper
+                        //     href={`/blog/${data.topic}`}
+                        //     src={`data:image/png;base64,${imgData}`}
+                        //     topic={data.topic}
+                        //     content={data.content}
+                        //     key={i}
+                        // ></BlogWrapper>
+                        // blogsArr.push(blog)
+                        // setBlogs(blogsArr)
+                        // changeCount.current += 1
                     }
-                    // Get the cached database saved from previous fetched
-                    if(sessionStorage.getItem('fetched homepage') !== null){
-                        const tempArrs = JSON.parse(sessionStorage.getItem('fetched homepage'))
-                        const newFetchedBlogs = blogsArr
-                        blogsArr = []
-                        for(let i = 0; i < tempArrs.length; i++){ 
-                            const blogData = tempArrs[i].props
-                            blogsArr.push(<BlogWrapper
-                                href={blogData.href}
-                                src={blogData.src}
-                                topic={blogData.topic}
-                                content={blogData.content}
-                                key={i}
-                        ></BlogWrapper>)
-                        }
-                        for(let i = 0; i < newFetchedBlogs.length; i++){
-                            blogsArr.push(newFetchedBlogs[i])
-                        }
-                    }
+                    // // Get the cached database saved from previous fetched
+                    // if(sessionStorage.getItem('fetched homepage') !== null){
+                    //     const tempArrs = JSON.parse(sessionStorage.getItem('fetched homepage'))
+                    //     const newFetchedBlogs = blogsArr.current
+                    //     const arr = []
+                    //     for(let i = 0; i < tempArrs.length; i++){ 
+                    //         const blogData = tempArrs[i].props
+                    //         arr.push(<BlogWrapper
+                    //             href={blogData.href}
+                    //             src={blogData.src}
+                    //             topic={blogData.topic}
+                    //             content={blogData.content}
+                    //             key={i}
+                    //     ></BlogWrapper>)
+                    //     }
+                    //     for(let i = 0; i < newFetchedBlogs.length; i++){
+                    //         arr.push(newFetchedBlogs[i])
+                    //     }
+                    // }
                     // Store cached
-                    sessionStorage.setItem('fetched homepage', JSON.stringify(blogsArr))
-                    sessionStorage.setItem('last fetched', loadEnd)
-                    sessionStorage.setItem('load from', loadFrom)
-                    sessionStorage.setItem('fetched', true)
-                    sessionStorage.setItem('max blogs', latestId)
-                    if(latestId > blogsArr.length - 1) setMoreButton("display")
-                    else setMoreButton("hide")
+                    // sessionStorage.setItem('fetched homepage', JSON.stringify(blogsArr.current))
+                    // sessionStorage.setItem('last fetched', loadEnd)
+                    // sessionStorage.setItem('load from', loadFrom)
+                    // sessionStorage.setItem('fetched', true)
                 }).catch((err)=>console.log(err))
             }
             fetchData()
@@ -82,10 +94,10 @@ const Homepage = () =>{
         } else if (!isFetch){
             // Get the cached data
             const tempArrs = JSON.parse(sessionStorage.getItem('fetched homepage'))
-            const blogsArr = []
+            const arr = []
             for(let i = 0; i < tempArrs.length; i++){ 
                 const blogData = tempArrs[i].props
-                blogsArr.push(<BlogWrapper
+                arr.push(<BlogWrapper
                     href={blogData.href}
                     src={blogData.src}
                     topic={blogData.topic}
@@ -93,32 +105,57 @@ const Homepage = () =>{
                     key={i}
                 ></BlogWrapper>)
             }
-            setBlogs(blogsArr)
+            setBlogs(arr)
+            BLOGS_SIZE.current = arr.length
+            blogsArr.current = arr
+            setBlogCount(arr.length)
+            setFetch(true)
             const maxBlogs = (sessionStorage.getItem('max blogs') === null) ? 0 : parseInt(sessionStorage.getItem('max blogs'))
-            if(maxBlogs > blogsArr.length - 1) setMoreButton("display")
+            if(maxBlogs > arr.length - 1) {
+                setMoreButton("display")
+            }
             else setMoreButton('hide')
+            sessionStorage.setItem('max blogs', maxBlogs)
         }
-    }, [isFetch, loadEnd, loadFrom, changeCount])
+
+        if(isFetch && blogCount === BLOGS_SIZE.current){
+            sessionStorage.setItem('fetched homepage', JSON.stringify(blogsArr.current))
+            sessionStorage.setItem('last fetched', loadEnd)
+            sessionStorage.setItem('load from', loadFrom)
+            sessionStorage.setItem('fetched', true)
+            sessionStorage.setItem('max blogs', latestId.current)
+            if(latestId.current > blogsArr.current.length - 1) setMoreButton("display")
+            else setMoreButton("hide")
+            setBlogs(blogsArr.current)
+        }
+    }, [isFetch, loadEnd, loadFrom, blogCount, blogs])
     
-    return(
-        <div style={{"textAlign": "center"}}>
-            <h1 style={{}}>Let's emerge and enjoy in music world today!</h1>
-            <h2>Homepage</h2>
-            <div className="homepage_button_wrapper">
-                <ExploreButton onClick={()=>console.log(1)}></ExploreButton>
-                <RandomPlaylistButton></RandomPlaylistButton>
-                <QuizButton></QuizButton>
+    if(!isFetch || blogCount !== BLOGS_SIZE.current){
+        return(
+            <LoadingPage></LoadingPage>
+        )
+    }
+    else{
+        return(
+            <div style={{"textAlign": "center"}}>
+                <h1 style={{}}>Let's emerge and enjoy in music world today!</h1>
+                <h2>Homepage</h2>
+                <div className="homepage_button_wrapper">
+                    <ExploreButton onClick={()=>console.log(1)}></ExploreButton>
+                    <RandomPlaylistButton></RandomPlaylistButton>
+                    <QuizButton></QuizButton>
+                </div>
+                <h2 style={{"margin": "25px 5px"}}>News</h2>
+                {blogs}
+                <MoreButton className={`${moreButton}`} onClick={()=>{
+                    setLoadFrom(loadEnd + 1)
+                    setLoadEnd(loadEnd + 3)
+                    sessionStorage.setItem('fetched', null)
+                    setFetch(false)
+                }}></MoreButton>
             </div>
-            <h2 style={{"margin": "25px 5px"}}>News</h2>
-            {blogs}
-            <MoreButton className={`${moreButton}`} onClick={()=>{
-                setLoadFrom(loadEnd + 1)
-                setLoadEnd(loadEnd + 3)
-                sessionStorage.setItem('fetched', null)
-                setFetch(false)
-            }}></MoreButton>
-        </div>
-    )
+        )
+    }
 }
 
 export default Homepage;
